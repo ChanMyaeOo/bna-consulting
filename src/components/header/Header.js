70ea2fc8ca426f69548644b25ed36013de79f616
ChanMyaeOo <chanmyaeoo1221gg@gmail.com>
2021-07-07 13:12:08 +0630

    added header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,122 @@
 import React from "react";
+import MenuIcon from "@material-ui/icons/Menu";
+import CloseIcon from "@material-ui/icons/Close";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import PhoneAndroidIcon from "@material-ui/icons/PhoneAndroid";
+import PhoneIcon from "@material-ui/icons/Phone";
+import EmailIcon from "@material-ui/icons/Email";
+import LogoImg from "../../images/bnatp.png";
+import ResLogoImg from "../../images/bna.png";
 import useStyles from "./styles";
 
 const Header = () => {
     const classes = useStyles();
-    return <div className={classes.header}>header</div>;
+    const handleCloseMenu = () => {
+        const resOverlay = document.getElementById("res-overlay");
+        resOverlay.style.display = "none";
+    };
+
+    const handleOpenMenu = () => {
+        const resOverlay = document.getElementById("res-overlay");
+        resOverlay.style.display = "block";
+    };
+    return (
+        <>
+            {/* For iPad View */}
+            <MenuIcon className={classes.menuIcon} onClick={handleOpenMenu} />
+            <div className={classes.resOverlay} id="res-overlay">
+                <div className={classes.resMenu}>
+                    <CloseIcon
+                        className={classes.closeMenu}
+                        onClick={handleCloseMenu}
+                    />
+                    <div className={classes.resLogoImgWrap}>
+                        <img
+                            src={ResLogoImg}
+                            alt="logo"
+                            className={classes.resLogoImg}
+                        />
+                    </div>
+
+                    <ul className={classes.resHeaderTop}>
+                        <li>
+                            <PhoneAndroidIcon fontSize="small" />
+                            0208889991012
+                        </li>
+                        <li>
+                            <PhoneIcon fontSize="small" />
+                            0208889991012
+                        </li>
+                        <li>
+                            <EmailIcon fontSize="small" />
+                            info@bnaconsulting.co.uk
+                        </li>
+                    </ul>
+
+                    <ul className={classes.resHeaderBottom}>
+                        <li>Home</li>
+                        <li>About Us</li>
+                        <li>Services</li>
+                        <li>Faqs</li>
+                        <li>Contact</li>
+                    </ul>
+
+                    <div className={classes.resHeaderSocials}>
+                        <FacebookIcon className={classes.resSocial} />
+                        <LinkedInIcon className={classes.resSocial} />
+                        <InstagramIcon className={classes.resSocial} />
+                        <EmailIcon className={classes.resSocial} />
+                    </div>
+                </div>
+            </div>
+
+            {/* For mobile view */}
+            <div className={classes.mobResMenuWrapper}>
+                <div className={classes.mobResMenu}>
+                    <div>
+                        <MenuIcon className={classes.mobMenuIcon} onClick={handleOpenMenu} />
+                    </div>
+
+                    <div className={classes.mobLogoWrap}>
+                    <img
+                        src={ResLogoImg}
+                        alt="logo"
+                        className={classes.mobLogoImg}
+                    />
+                    </div>
+                </div>
+            </div>
+
+            {/* For Laptop View */}
+            <div className={classes.header}>
+                <div className={classes.headerLogoWrap}>
+                    <img
+                        src={LogoImg}
+                        alt="logo"
+                        className={classes.headerLogo}
+                    />
+                </div>
+
+                <div className={classes.headerLinks}>
+                    <ul className={classes.headerTopLinks}>
+                        <li>info@bnaconsulting.co.uk</li>
+                        <li>0208889991012</li>
+                        <li>1 Kingdom Street, Paddington, W2 6BD</li>
+                    </ul>
+
+                    <ul className={classes.headerBtnLinks}>
+                        <li>Home</li>
+                        <li>About Us</li>
+                        <li>Services</li>
+                        <li>Faqs</li>
+                        <li>Contact</li>
+                    </ul>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default Header;
--- src/components/header/styles.js
+++ src/components/header/styles.js
@@ -2,8 +2,159 @@ import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
     header: {
-        color: 'green'
-    }
+        display: 'flex',
+    },
+    headerLogoWrap: {
+        width: '25%',
+        display: 'flex',
+        alignItems: 'center'
+    },
+    headerLogo: {
+        width: '140px',
+        height: '85px',
+        marginLeft: '50px'
+    },
+    headerLinks: {
+        width: '75%',
+        display: 'flex',
+        flexDirection: 'column',
+        marginLeft: 'auto',
+    },
+    headerTopLinks: {
+        display: 'flex',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        background: '#000',
+        height: '50px',
+        color: '#fff',
+        "& > li": {
+            listStyle: 'none'
+        }
+    },
+    headerBtnLinks: {
+        display: 'flex',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        height: '65px',
+        "& > li": {
+            listStyle: 'none',
+            fontWeight: 'bold'
+        }
+    },
+    menuIcon: {
+        fontSize: '40px',
+        border: '1px solid #000',
+        borderRadius: '50%',
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        display: 'none'
+    },
+
+    closeMenu: {
+        fontSize: '50px',
+        color: '#fff',
+        cursor: 'pointer'
+    },
+
+    // Responsive menu 
+    resOverlay: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        background: "rgba(0,0,0,0.7)",
+        display: 'none',
+    },
+    resMenu: {
+        background: '#000',
+        height: '100%',
+        width: '250px',
+    },
+    resLogoImgWrap: {
+        display: 'flex',
+        justifyContent: 'center',
+        marginBottom: '25px'
+    },
+    resLogoImg: {
+        width: '190px',
+        height: '100px',
+    },
+    contactSocial: {
+        lineHeight: '14px'
+    },
+    resHeaderTop: {
+        color: '#6b521c',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        "& > li": {
+            listStyle: 'none',
+            fontSize: '14px'
+        }
+    },
+    resHeaderBottom: {
+        color: '#fff',
+        "& > li": {
+            listStyle: 'none',
+            margin: '30px'
+        }
+    },
+    resHeaderSocials: {
+        display: 'flex',
+        justifyContent: 'space-around',
+        marginTop: '40px'
+    },
+    resSocial: {
+        color: '#6b521c'
+    },
+
+    // For mobile view
+    mobResMenuWrapper: {
+        display: 'none'
+    },
+    mobResMenu: {
+        height: '62px',
+        width: '100%',
+        background: '#000',
+        display: 'flex',
+        "& > div": {
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+        }
+    },
+    mobLogoWrap: {
+        width: '100%',
+        margin: '0 auto'
+    },
+    mobLogoImg: {
+        width: '95px',
+        height: '50px',
+        margin: '0 auto'
+    },
+    mobMenuIcon: {
+        color: '#fff',
+        fontSize: '40px'
+    },
+  
+    [theme.breakpoints.down('md')]: {
+        menuIcon: {
+            display: 'block'
+        },
+        header: {
+            display: 'none'
+        }
+    },
+    [theme.breakpoints.down('xs')]: {
+        menuIcon: {
+            display: 'none'
+        },
+        mobResMenuWrapper: {
+            display: 'block'
+        }
+    },
 }));
 
 export default useStyles;
